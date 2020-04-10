affcafd3e7146f60e438e893eeaef9231591323a
Carlos Leonardo Camilo Vargas Huaman <carlos.vargas.huaman@avantica.net>
2020-04-10 15:45:42 -0500

    Add filterIndexedTo

---
+++ src/com/carlitosdroid/filterIndexedTo.kt
@@ -0,0 +1,21 @@
+package com.carlitosdroid
+
+object filterIndexedTo {
+
+    /**
+     * The [filterIndexedTo] function returns a C (Collection), this returns a filtered Mutable Collection
+     * the first parameter 'names2' also will contain this filtered collection and must be a Mutable Collection
+     * the original list will remain intact
+     */
+    @JvmStatic
+    fun main(args: Array<String>) { // write your code here
+        val names1 = listOf("Eduardo", "Leonardo", "Andy", "Jose")
+        val names2 = mutableListOf<String>()
+        val names3 = names1.filterIndexedTo(names2) { index, element ->
+            (index == 0 && element.length > 4)
+        }
+        println("remains the same: $names1")
+        println("remains the same: $names2")
+        println("remains the same: $names3")
+    }
+}
