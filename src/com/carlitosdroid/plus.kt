1590072ea80fd557fd2addb26bb3419f37d8865a
Carlos Leonardo Camilo Vargas Huaman <carlos.vargas.huaman@avantica.net>
2020-04-10 00:21:47 -0500

    Add plus

--- src/com/carlitosdroid/plus.kt
+++ src/com/carlitosdroid/plus.kt
@@ -4,7 +4,10 @@ object plus {
 
     @JvmStatic
     fun main(args: Array<String>) { // write your code here
-
+        val names1 = mutableSetOf("Carlos", "Ronaldo", "Isaias", "Maria")
+        val names2 = listOf("Carlos", "Grecia", "Yuriko")
+        val result = names1.plus(names2)
+        print(result)
     }
 
 }
--- src/com/carlitosdroid/plusAssign.kt
+++ src/com/carlitosdroid/plusAssign.kt
@@ -6,7 +6,7 @@ object plusAssign {
     fun main(args: Array<String>) { // write your code here
         val names1 = mutableSetOf("Carlos", "Ronaldo", "Isaias", "Maria")
         val names2 = mutableListOf("Carlos", "Grecia", "Yuriko")
-        names1.plus(names2)
+        names1.plusAssign(names2)
         print(names1)
     }
 
