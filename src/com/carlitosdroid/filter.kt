219d245aea3715ed4ad022ef76af26c6515adf6d
Carlos Leonardo Camilo Vargas Huaman <carlos.vargas.huaman@avantica.net>
2020-04-10 15:26:23 -0500

    Add filterIndexed

--- src/com/carlitosdroid/filter.kt
+++ src/com/carlitosdroid/filter.kt
@@ -1,6 +1,12 @@
 package com.carlitosdroid
 
 object filter {
+
+    /**
+     * The [filter] function returns a List,
+     * when you call it, you must assign the returned List to a new variable
+     * if you just call it and don't return anything, the list remains the same
+     */
     @JvmStatic
     fun main(args: Array<String>) { // write your code here
         val names = mutableSetOf("Eduardo", "Leonardo", "Andy", "Jose")
@@ -11,7 +17,7 @@ object filter {
         val filterNames = names.filter {
             it.length > 4
         }
-        println(filterNames)
+        println("this is the filter list: $filterNames")
     }
 
 }
--- src/com/carlitosdroid/filterIndexed.kt
+++ src/com/carlitosdroid/filterIndexed.kt
@@ -1,4 +1,22 @@
 package com.carlitosdroid
 
 object filterIndexed {
+
+    /**
+     * The [filterIndexed] function returns a List, the benefit is that is provides you every index
+     * when you call it, you must assign the returned List to a new variable
+     * if you just call it and don't return anything, the list remains the same
+     */
+    @JvmStatic
+    fun main(args: Array<String>) { // write your code here
+        val names = listOf("Eduardo", "Leonardo", "Andy", "Jose")
+        names.filterIndexed { index, element ->
+            (index == 0 && element.length > 4)
+        }
+        println("remains the same: $names")
+        val filterNames = names.filterIndexed {index, element ->
+            index == 0 && element.length > 4
+        }
+        println("this is the filter list: $filterNames")
+    }
 }
