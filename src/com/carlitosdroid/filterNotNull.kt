b3fa5450683682bc03208409fc11942916bc95cf
Carlos Leonardo Camilo Vargas Huaman <carlos.vargas.huaman@avantica.net>
2020-04-10 16:07:52 -0500

    Add filterNotNull

--- src/com/carlitosdroid/filterNotNull.kt
+++ src/com/carlitosdroid/filterNotNull.kt
@@ -1,21 +1,17 @@
 package com.carlitosdroid
 
-object filterNot {
+object filterNotNull {
 
     /**
-     * The [filterNot] function returns a List.
+     * The [filterNotNull] function returns a List
      * you must assign the response to a new variable
      */
     @JvmStatic
     fun main(args: Array<String>) { // write your code here
         val names1 = listOf("Eduardo", null, "Mari", null, "Jose", "Juan")// list of nullable String
-        names1.filterNot {
-            it == null
-        }
+        names1.filterNotNull()
         println("It remains the same: $names1")
-        val names2 = names1.filterNot {
-            it == null
-        }
+        val names2 = names1.filterNotNull()
         println("$names2")
     }
 }
