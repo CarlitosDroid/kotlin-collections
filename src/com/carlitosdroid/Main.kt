8aacc0e990121d061a2e8f1be9fa45d068a608d6
Carlos Leonardo Camilo Vargas Huaman <carlos.vargas.huaman@avantica.net>
2020-04-10 21:35:45 -0500

    Add generated files to be able for running

--- src/com/carlitosdroid/Main.kt
+++ src/com/carlitosdroid/Main.kt
@@ -1,23 +1,7 @@
 package com.carlitosdroid
 
 object Main {
-
     @JvmStatic
     fun main(args: Array<String>) { // write your code here
-        val words = "A long time ago in a galaxy far far away".split(" ")
-        val shortWords = mutableListOf<String>()
-        words.getShortWords(shortWords, 3)
-        println(shortWords)
     }
-
-    private fun List<String>.getShortWords(shortWords: MutableList<String>, maxLength: Int) {
-
-//        this.filter(shortWords) {
-//            it.length <= maxLength
-//        }
-//        println(shortWords)
-//        val articles = setOf("a", "A", "an", "An", "the", "The")
-//        shortWords -= articles
-    }
-
 }
